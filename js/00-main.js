9050c564776d20839f9fff0a84297341e64b52ce
Peter French <peterfrench94@gmail.com>
2024-04-11 15:24:42 +0200

    Adds fixes for broken top navigation (#27)
    * Adds fixes for broken top navigation
    
    * Refactor to CSS

--- js/00-main.js
+++ js/00-main.js
@@ -1,25 +1,38 @@
-jQuery( function () {
-	const INTERNAL_EVENTS_CATEGORY = 4;
+document.addEventListener("DOMContentLoaded", function () {
+	// Check if we are in the room booking page ('/rooms/book') as it is not compatible with JQuery
+	const currentUrl = window.location.pathname;
+	if (currentUrl != "/rooms/book" && currentUrl != "/rooms") {
+		jQuery(function () {
+			const INTERNAL_EVENTS_CATEGORY = 4;
 
-	// moves the menu header
-	jQuery('.toolbar.global-menu').detach().appendTo('div.page-header>div>a');
+			// moves the menu header
+			const $toolbar = jQuery('.toolbar.global-menu').detach();
+			const $targetDiv = jQuery('div.page-header>div');
+			const $logo = $targetDiv.find('a').first();
+			$toolbar.insertAfter($logo);
 
-	// display moved header
-	jQuery('div.page-header').css({display: 'block'});
+			// update administration button text
+			const administrationBtn = $toolbar.find('a:contains("Administration")');
+			if (administrationBtn.length) {
+				administrationBtn.text('Admin settings');
+			}
 
-	// a hack to change the accept text for the internal events category
-	let confActionAcceptLabel = document.querySelector(".conference-page .action-box.accept .label");
-	let categoryLink = document.querySelector(".main-action-bar a.icon-collapse");
-	let categoryLinkHref = "/category/" + INTERNAL_EVENTS_CATEGORY + "/";
-	if (confActionAcceptLabel && categoryLink && categoryLink.getAttribute("href") == categoryLinkHref) {
-		confActionAcceptLabel.textContent = "You have been registered for this event.";
-		if (document.querySelector(".action-box.accept .i-button.icon-edit")) {
-			confActionAcceptLabel.textContent += " Please make sure all the below details are completed.";
-		} else {
-			confActionAcceptLabel.textContent += " For changes to your details, please reach out to the event manager.";
-		}
+			// display moved header
+			jQuery('div.page-header').css({ display: 'block' });
+
+			// a hack to change the accept text for the internal events category
+			let confActionAcceptLabel = document.querySelector(".conference-page .action-box.accept .label");
+			let categoryLink = document.querySelector(".main-action-bar a.icon-collapse");
+			let categoryLinkHref = "/category/" + INTERNAL_EVENTS_CATEGORY + "/";
+			if (confActionAcceptLabel && categoryLink && categoryLink.getAttribute("href") == categoryLinkHref) {
+				confActionAcceptLabel.textContent = "You have been registered for this event.";
+				if (document.querySelector(".action-box.accept .i-button.icon-edit")) {
+					confActionAcceptLabel.textContent += " Please make sure all the below details are completed.";
+				} else {
+					confActionAcceptLabel.textContent += " For changes to your details, please reach out to the event manager.";
+				}
+			}
+		});
 	}
-
-	// set the display to visible now that the js has been parsed
-	jQuery(document.body).css({visibility: 'visible'});
+	document.querySelector("body").style.visibility = "visible";
 });
